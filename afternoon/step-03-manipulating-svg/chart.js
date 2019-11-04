ef717be3b519c6f0fe15d2eb315c997b6c3c8d8e
John Muyskens <jmuyskens@gmail.com>
2019-11-04 00:09:51 -0500

    added workshop steps

---
+++ afternoon/step-03-manipulating-svg/chart.js
@@ -0,0 +1,55 @@
+// Margin convention
+let width = 300
+let height = 400
+
+// because we are hot loading:
+d3.selectAll('svg').remove()
+
+let svg = d3.select('div#chart')
+  .append('svg')
+    .attr('width', width + margin.right + margin.left)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+let snowBody = svg.append('g')
+  .attr('fill', 'white')
+  .attr('stroke', 'black')
+
+snowBody.append('circle')
+  .attr('r', 60)
+  .attr('cx', 150)
+  .attr('cy', 230)
+
+snowBody.append('circle')
+  .attr('r', 40)
+  .attr('cx', 150)
+  .attr('cy', 150)
+
+snowBody.append('circle')
+  .attr('r', 30)
+  .attr('cx', 150)
+  .attr('cy', 90)
+
+svg.append('ellipse')
+  .attr('rx', 3)
+  .attr('ry', 5)
+  .attr('cx', 140)
+  .attr('cy', 80)
+
+svg.append('ellipse')
+  .attr('rx', 3)
+  .attr('ry', 5)
+  .attr('cx', 160)
+  .attr('cy', 80)
+
+svg.append('path')
+  .attr('d', `M 150,90
+              L 170,95
+              L 150,100 z`)
+
+svg.append('text')
+  .attr('x', 150)
+  .attr('y', 40)
+  .attr('text-anchor', 'middle')
+  .text('Hello, world!')
---
+++ afternoon/step-06-bar-chart/chart.js
@@ -0,0 +1,65 @@
+// Margin convention
+let margin = {top: 10, right: 10, left: 200, bottom: 10}
+let width = 700 - margin.right - margin.left
+let height = 500 - margin.top - margin.bottom
+
+// because we are hot loading:
+d3.selectAll('svg').remove()
+
+let svg = d3.select('div#chart')
+  .append('svg')
+    .attr('width', width + margin.right + margin.left)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+let scale = d3.scaleLinear()
+  .range([0, width])
+
+let render = (raw) => {
+  let data = raw
+    .sort((b, a) => +a.healthExpPerCapita - +b.healthExpPerCapita)
+    .slice(0, 15)
+
+  let maxValue = d3.max(data, d => +d.healthExpPerCapita)
+
+  scale.domain([0, maxValue])
+
+  let bars = svg.selectAll('rect')
+    .data(data)
+    .enter().append('rect')
+    .style('fill', '#000')
+    .attr('width', d => scale(+d.healthExpPerCapita))
+    .attr('height', 20)
+    .attr('transform', (d, i) => `translate(0, ${i * 25})`)
+
+  let text = svg.selectAll('text.country-label')
+    .data(data)
+    .enter().append('text')
+    .attr('class', 'country-label')
+    .attr('y', (d, i) => i * 25)
+    .attr('dx', -3)
+    .attr('dy', 16)
+    .style('text-anchor', 'end')
+    .text(d => d.name)
+
+}
+
+d3.csv('../../data/oecd.csv').then(raw => {
+  let data = raw.map(d => {
+    d.healthExpPerCapita = +d.healthExpPerCapita
+    d.year = +d.year
+    return d
+  })
+
+  let dataYears = d3.nest()
+    .key(function(d) { return d.year })
+    .map(data)
+
+  let dataCountries = d3.nest()
+    .key(function(d) { return d.name })
+    .entries(data)
+
+  let year = 1970
+  render(dataYears['$' + year])
+})
